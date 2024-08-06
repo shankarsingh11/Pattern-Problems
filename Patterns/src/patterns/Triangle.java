bd652efab3b7358a16484ee413eb66416857d3dc
Shankar Singh <shankarrajputsingh.256@gmail.com>
2024-08-06 13:45:15 +0530

    first commit

---
+++ Patterns/src/patterns/Triangle.java
@@ -0,0 +1,21 @@
+package patterns;
+import java.util.*;
+
+public class Triangle {
+
+	public static void main(String[] args) {
+	
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter the Row number");
+	   int  n = sc.nextInt();
+	
+		for(int i = 0;i<n;i++) {
+			
+			 for(int j = 0;j<=i;j++) {
+				 System.out.print(" * ");
+			 }
+			 System.out.println();
+		}
+	}
+
+}
